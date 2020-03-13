1d851863893a7430891c472d24c9ed219ce759e5
wujidadi <wujidadi@gmail.com>
2020-03-13 19:37:59 +0800

    增訂 prototype.js

--- prototype.js
+++ prototype.js
@@ -1,3 +1,9 @@
+/*
+ *==============================
+ * 字串相關方法
+ *==============================
+ */
+
 /**
  * 將已轉成字串的秒數轉換為 HH:MM:SS 格式
  * @returns {string} HH:MM:SS 格式的秒數
@@ -19,16 +25,30 @@ String.prototype.toHHMMSS = function() {
     }
 
     return Hour + ':' + Minute + ':' + Second;
-}
+};
+
+
+/*
+ *==============================
+ * 物件相關方法
+ *==============================
+ */
 
 /**
- * 清空所有子元素（在 `Element.prototype` 中添加的方法）
+ * 取得物件屬性數量
+ * 透過 `Object.defineProperty` 在 `Object.prototype` 中添加的方法，ES5 以後適用
  */
-Element.prototype.removeAllChildren = function() {
-    while (this.firstChild) {
-        this.removeChild(this.firstChild);
-    }
-};
+Object.defineProperty(Object.prototype, 'length', {
+    get: function() {
+        let obj = this,
+            len = 0;
+        for (let key in obj) {
+            len++;
+        }
+        return len;
+    },
+    enumerable: false
+});
 
 /**
  * 清空物件（透過 `Object.defineProperty` 在 `Object.prototype` 中添加的方法，ES5 以後適用）
@@ -42,3 +62,19 @@ Object.defineProperty(Object.prototype, 'removeAllProperties', {
     },
     enumerable: false
 });
+
+
+/*
+ *==============================
+ * 元素相關方法
+ *==============================
+ */
+
+/**
+ * 清空所有子元素（在 `Element.prototype` 中添加的方法）
+ */
+Element.prototype.removeAllChildren = function() {
+    while (this.firstChild) {
+        this.removeChild(this.firstChild);
+    }
+};
